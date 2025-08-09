97265a41343d3e97ce08896e28845f27bf5d18f6
James Lazo (jazo) <james.lazo+git@protonmail.com>
2025-08-09 15:43:52 -0700

    chore: update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,14 @@
 # pacman -Syu git
 # mkdir .config && cd .config
 # git clone https://github.com/jameslazo/arch-lxc .
-# chmod +x script.sh && ./script.sh
+# chmod +x setup.sh && ./setup.sh
 
-. bash/.bashrc
-pacman -S --noconfirm tmux vim man-pages bash-completion
+. ~/.config/bash/.bashrc
+pacman -S --noconfirm tmux vim man-pages bash-completion cronie
+systemctl enable cronie --now
+mkdir ~/sh
+echo -e "#!/bin/bash\npacman -Syu --noconfirm\nshutdown -r now" > ~/sh/upboot.sh
+chmod +x ~/sh/upboot.sh
+echo "01 0 * * sun /root/sh/upboot.sh" >> /etc/crontab
+
+
